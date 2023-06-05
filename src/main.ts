57ae1bc43d0f1e692376ebb94868daa045039cf5
WilhelmYakunin <v.jakunin@inbox.ru>
2023-06-05 19:49:47 +0300

    fix: fix lint warring

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { AppModule } from './app.module';
 
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import * as yaml from 'yaml';
 import * as path from 'path';
 import { readFile } from 'node:fs/promises';
